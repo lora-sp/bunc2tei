d4bb722e50f185e06d57bb3443473687115f0a91
lora-sp <lora.spassova@swhk.ids-mannheim.de>
2023-03-13 13:55:38 +0100

    remove comments

--- escape.py
+++ escape.py
@@ -4,7 +4,6 @@ import xml.etree.ElementTree as ET
 
 def escape(file):        
     with open(sys.argv[i], "r+") as f:
-        print("opened")
         file = f.read()
         file = re.sub('&(?!gt;|apos;|quot;|lt;|amp;)', '&amp', file)
         f.seek(0)
@@ -19,15 +18,4 @@ for i in range(1, len(sys.argv)):
         #ET.parse(sys.argv[i])
         #print("sucess")
 
-
-
-
-#for i in range(1, len(sys.argv)):
-#    try:
-#        ET.parse(sys.argv[i])
-#        print("parsed")
-#    except:
-#        print("not parsed")
-#        replace(sys.argv[i])
-        
 
