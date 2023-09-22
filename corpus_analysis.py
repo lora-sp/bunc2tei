ed93dd290db583c1ac7754644ac5a47c50da316d
lora-sp <lora.spassova@swhk.ids-mannheim.de>
2023-09-22 13:11:26 +0200

    update

--- corpus_analysis.py
+++ corpus_analysis.py
@@ -2,7 +2,18 @@ from KorAPClient import KorAPConnection
 bunc = KorAPConnection(KorAPUrl = "http://localhost:64543/", verbose=True)
 dereko = KorAPConnection(verbose = True)
 
-### 1.: searching for LVCs in both languages in a collocational analysis
+
+### 1.: comparing the word много in its noun and adjective modifying functions in Bulgarian
+
+q = bunc.corpusQuery("много [ud/p=NOUN]", metadataOnly=False)
+q = q.fetchAll(verbose=True)
+print(q)
+
+r = bunc.corpusQuery("много [ud/p=ADJ]", metadataOnly=False)
+r = r.fetchAll(verbose=True)
+print(r)
+
+### 2.: searching for LVCs in both languages in a collocational analysis
 
 vlizam = bunc.collocationAnalysis("focus({[ud/l=влизам] в} [ud/p=NOUN])",
 					leftContextSize = 0,
@@ -21,13 +32,5 @@ treten = dereko.collocationAnalysis("focus(in [tt/p=NN] {[tt/l=treten]})",
 
 
 
-###2.: comparing the word много in its noun and adjective modifying functions in Bulgarian
 
-q = bunc.corpusQuery("много [ud/p=NOUN]", metadataOnly=False)
-q = q.fetchAll(verbose=True)
-print(q)
-
-r = bunc.corpusQuery("много [ud/p=ADJ]", metadataOnly=False)
-r = r.fetchAll(verbose=True)
-print(r)
 
