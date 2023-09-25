a14d001394ddb4ad52322e71523fac7fd444fa71
Lora Spassova <74067841+lora-sp@users.noreply.github.com>
2023-09-25 22:41:03 +0200

    Update escape.py

--- escape.py
+++ escape.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 
 
 def escape(file):        
+	''' Reads every file from the standard input and looks for unescaped ampersand ('&') and less-than ('<') symbols 
+ 	wit the help of a regular expression and replaces them with '&amp;' and '&lt;', respectively '''
 	with open(sys.argv[i], "r+") as f:
 		file = f.read()
 		replace_symbols = r'&(?!gt;|apos;|quot;|lt;|amp;)|<(?!\/|p>|text|body|div|TEI|\?xml)'
@@ -22,7 +24,7 @@ def escape(file):
 	
 
 for i in tqdm(range(1, len(sys.argv))):
-
+	''' Prints names of the files handled by the escape function. Print message if parsing still fails after escaping. '''
 	try:
 		ET.parse(sys.argv[i])
 	except:
